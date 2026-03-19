76e60a3ea1ace2cd4da243b4122c2062195264e9
grandaoInBytes <lucanontelodico@gmail.com>
2026-03-20 00:30:06 +0100

    Updated pics

--- script.js
+++ script.js
@@ -123,18 +123,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   };
 
-  // --- INITIALIZE BACKGROUND (No changes here, copied from original) ---
-  const backgroundImages = [
-    'images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg',
-    'images/image4.jpg', 'images/image5.jpg', 'images/image6.jpg',
-    'images/image7.jpg', 'images/image8.jpg', 'images/image9.jpg',
-    'images/image10.jpg', 'images/image11.jpg', 'images/image12.jpg',
-    'images/image13.jpg', 'images/image14.jpg', 'images/image15.jpg',
-    'images/image16.jpg', 'images/image17.jpg', 'images/image18.jpg',
-    'images/image19.jpg', 'images/image20.jpg', 'images/image21.jpg',
-    'images/image22.jpg', 'images/image23.jpg', 'images/image24.jpg',
-    'images/image25.jpg', 'images/image26.jpg',
-  ];
+  // Set the total number of images you have in your folder
+  const totalImages = 32; 
+  // This creates the array ['images/image1.jpg', 'images/image2.jpg', ...] automatically
+  const backgroundImages = Array.from({ length: totalImages }, (_, i) => `images/image${i + 1}.jpg`);
+  
   const backgroundContainer = document.querySelector('.background');
   const switchInterval = 4000;
   let previousIndex = -1;
